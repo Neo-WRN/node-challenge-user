5c3c335381a6d86b01df17c37eb4dc6acb380c84
Neo-WRN <nicolas.destito@warren.com.br>
2022-04-13 15:03:42 -0300

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { readFile } = require('fs').promises;
+
+
+const app = express();
+const router = express.Router()
+
+// Routes
+app.post('/customer', async (request, response) => {
+    
+});
