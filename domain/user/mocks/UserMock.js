593f92143487eab65a53d536ef932a817d7ddefc
Neo-WRN <nicolas.destito@warren.com.br>
2022-04-21 12:55:07 -0300

    created UserMock.js, createCustomer saves new user to UserMock's Users variable

---
+++ domain/user/mocks/UserMock.js
@@ -0,0 +1,23 @@
+const Users = []
+module.exports = {Users}
+// class UserMock {
+//     //TODO See if there is a better way to do this constructor
+//     //TODO See if the properties should be private (and then acessed or used though a get and set)
+    
+//     constructor(full_name, email, email_confirmation, cpf, 
+//         cellphone, birthdate, email_sms, whatsapp, 
+//         country, city, postal_code, address) {
+//             this.full_name = full_name
+//             this.email = email
+//             this.email_confirmation = email_confirmation
+//             this.cpf = cpf
+//             this.cellphone = cellphone
+//             this.birthdate = birthdate
+//             this.email_sms = email_sms
+//             this.whatsapp = whatsapp
+//             this.country = country
+//             this.city = city
+//             this.postal_code = postal_code
+//             this.address = address
+//         }
+// }
--- presentation/controllers/UserController.js
+++ presentation/controllers/UserController.js
@@ -1,5 +1,5 @@
 const { validationResult } = require('express-validator')
-
+const {Users} = require('../../domain/user/mocks/UserMock')
 class UserController {
 
     // registerPage(req, res, next) {
@@ -10,7 +10,16 @@ class UserController {
         if(!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.mapped()})
         }
-        console.log(req.body)
+        const {full_name, email, email_confirmation, cpf, 
+            cellphone, birthdate, email_sms, whatsapp, 
+            country, city, postal_code, address} = req.body
+        const newUser = [full_name, email, email_confirmation, cpf, 
+            cellphone, birthdate, email_sms, whatsapp, 
+            country, city, postal_code, address]
+        
+        Users.push(newUser)
+        console.log(Users)
+        
         res.status(200).end('success')
     }
 }
